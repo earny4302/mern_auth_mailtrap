81dde0fa57c2a963ee7cb39a8dec92739ba755c9
earny4302 <73746161+earny4302@users.noreply.github.com>
2024-08-18 23:19:14 +0530

    completed database setup

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import express from "express";
-
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
+dotenv.config();
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send("hello world123");
-})
-app.listen(3000,()=>{
-    console.log("listening on port 3000");
+const PORT = process.env.PORT || 5000;
+app.use("/api/auth", authRoutes)
+app.listen(PORT,()=>{
+    connectDB();
+    console.log("listening on port: " , PORT);
 })
 // password:auth_tut
