1e8184e8dae6403a2ba088b9a61085402b9f9447
earny4302 <73746161+earny4302@users.noreply.github.com>
2024-08-18 21:57:10 +0530

    INITIAL BACKEND

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-im
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("hello world123");
+})
+app.listen(3000,()=>{
+    console.log("listening on port 3000");
+})
+// password:auth_tut
