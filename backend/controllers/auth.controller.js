733b6709f6f6d4c56c5ab496e6f59b1cfd4335ac
earny4302 <73746161+earny4302@users.noreply.github.com>
2024-08-21 09:33:33 +0530

    ROUTING

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,20 @@
+import {User} from '../models/user.model.json';
+import bcryptjs from 'bcryptjs';
 export const signup = async(req, res ) => {
-    res.send("signup route");
+    const {email, password, name }= req.body;
+    try{
+        if(email|| password|| name){
+            throw new Error("All fields are required");
+        }
+        const userAlreadyExists = await User.findOne({email});
+        if(userAlreadyExists)
+        {
+            return res.status(400).json({success: false, message: "User already exists"});
+        }
+        const hashedPassword = await bcryptjs.hash(password,10);
+    } catch(error) {
+
+    }
 
 };
 
--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,9 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+
+app.use(express.json());
 app.listen(PORT,()=>{
     connectDB();
     console.log("listening on port: " , PORT);
